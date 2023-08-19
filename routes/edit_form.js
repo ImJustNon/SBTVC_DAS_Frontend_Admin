e875cca10a730940824c57165a8416ee2db7b101
ImJustNon <Im_Just_Non@hotmail.com>
2023-08-19 18:41:19 +0700

    Update add edit form page

---
+++ routes/edit_form.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { connection } = require("../database/connect.js");
+const router = express.Router();
+
+const { check_login } = require("../middleware/check_login.js");
+const axios = require("axios");
+
+
+router.get("/p/edit-form", check_login, async(req, res) =>{
+    return res.render("index.ejs", {
+        PAGE: "EDIT_FORM",  
+        session: {
+            is_login: req.session.is_login,
+            admin_id: req.session.admin_id,
+        },
+        data: {
+
+        },
+    });
+});
+
+module.exports = router;
