13360e4d42a46e6cae7f786a5fb809f1db3af9cc
ImJustNon <Im_Just_Non@hotmail.com>
2023-08-22 23:47:27 +0700

    add page => manage_form.ejs

--- routes/manage_form.js
+++ routes/manage_form.js
@@ -7,16 +7,34 @@ const axios = require("axios");
 
 
 router.get("/p/manage-form", check_login, async(req, res) =>{
-    return res.render("index.ejs", {
-        PAGE: "MANAGE_FORM",  
-        session: {
-            is_login: req.session.is_login,
-            admin_id: req.session.admin_id,
-        },
-        data: {
+    try {
+        const response = await axios.post('https://sbtvc-das-api.nonlnwza.xyz/api/admin/form/get_form_data', {
+            filter: "NO_FILTER"
+        }, {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+        });
 
-        },
-    });
+        return res.render("index.ejs", {
+            PAGE: "MANAGE_FORM",  
+            session: {
+                is_login: req.session.is_login,
+                admin_id: req.session.admin_id,
+            },
+            data: {
+                results: response.data.status === "SUCCESS" ? response.data.data.results : [],
+            },
+        });
+    }
+    catch(err){
+        return res.json({
+            status: "FAIL",
+            error: err,
+        });
+    }
+
+    
 });
 
 module.exports = router;
