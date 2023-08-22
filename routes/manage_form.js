8e77d98dcfdeb40940388ba32ae4f95d39bddae4
ImJustNon <Im_Just_Non@hotmail.com>
2023-08-22 22:22:25 +0700

    rename edit-form => manage-form

--- routes/manage_form.js
+++ routes/manage_form.js
@@ -6,9 +6,9 @@ const { check_login } = require("../middleware/check_login.js");
 const axios = require("axios");
 
 
-router.get("/p/edit-form", check_login, async(req, res) =>{
+router.get("/p/manage-form", check_login, async(req, res) =>{
     return res.render("index.ejs", {
-        PAGE: "EDIT_FORM",  
+        PAGE: "MANAGE_FORM",  
         session: {
             is_login: req.session.is_login,
             admin_id: req.session.admin_id,
